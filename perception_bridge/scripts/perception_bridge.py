d9e2a2f1f621af4935eac08241b1b39f8e6e5a49
xukunlin <1125290220@qq.com>
2019-11-16 17:36:21 +0800

    1，增加感知桥加载应用环境的功能

--- perception_bridge/scripts/perception_bridge.py
+++ perception_bridge/scripts/perception_bridge.py
@@ -49,16 +49,16 @@ class PerceptionSever():
 
                 self.cleanPlasm = ecto.Plasm()
                 self.cleanPlasm.connect(self.rosSource["output"] >> self.pclFunsion["input"], self.pclFunsion["output"] >> self.objectFilter["input"])
-	
-		self.keepRun = True
 
-                self.handle_clean("init")
+		self.keepRun = True
+                #self.handle_clean("init")
 
 	def start(self):
 		self.lookService = rospy.Service('look', Look, self.handle_look)
 		self.savePclService = rospy.Service('save_pcl', SavePCL, self.handle_save)
 		self.loadPclService = rospy.Service('load_pcl', LoadPCL, self.handle_load)
 		self.cleanPclService = rospy.Service('clean_pcl', CleanPCL, self.handle_clean)
+		self.uploadSceneService = rospy.Service('upload_scene', UploadScene, self.handle_uploadScene)
 
 	def handle_look(self, req):
 		print("in look service call back")
@@ -68,11 +68,14 @@ class PerceptionSever():
 		lock.release()
 		return LookResponse(0)
 
+	def handle_uploadScene(self, req):
+		print("upload the scene file")
+		print(os.system(req.fileName))
+		return UploadSceneResponse(0)
+
 	def handle_save(self, req):
 		print("saving pcl to pcd file")
 		print("file name = ", req.fileName)
-		#self.saver.params.filename_format = req.fileName
-
 		self.pclFunsion.params.save = True
 		self.sched = ecto.Scheduler(self.savePlasm)
 		lock.acquire()
